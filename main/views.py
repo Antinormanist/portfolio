98f7c1063fc90d3f1a7c293cbf29f78b03a98c00
Antinormanist <bogdanit.goryachev.2009@gmail.com>
2025-03-23 08:45:21 +1000

    he's ready

--- main/views.py
+++ main/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from .utils import send_email
-# Create your views here.
+
 def welcome(request):
     context = {
         'title': 'It\'s me',
@@ -14,26 +13,12 @@ def welcome_ru(request):
     return render(request, 'main/welcome-ru.html', context)
 
 def main(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-        if name and email and message:
-            send_email(name, email, message)
-
     context = {
         'title': 'It\'s me again!',
     }
     return render(request, 'main/main.html', context)
 
 def main_ru(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-        if name and email and message:
-            send_email(name, email, message)
-
     context = {
         'title': 'Это снова я!',
     }
